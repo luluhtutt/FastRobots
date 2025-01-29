ce7e0b9b8c833b9a9756214d6cfe9c7c5cf3d149
Lulu Htutt <lh543@cornell.edu>
2025-01-29 16:13:33 -0500

    updated website and added lab 1 stuff

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,3 +1,9 @@
 function toggleCard(card) {
-    card.classList.toggle("expanded");
+    card.classList.toggle('expanded');
+    // const toggleText = card.querySelector('.toggle-text');
+    // if (card.classList.contains('expanded')) {
+    //     toggleText.textContent = 'Click to collapse';
+    // } else {
+    //     toggleText.textContent = 'Click to expand';
+    // }
 }
